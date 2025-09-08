cfbea9ad344373797f57553136e83e8a7682211e
sorianokiah6-beep <sorianokiah6@gmail.com>
2025-09-08 13:44:37 +0800

    Create Seatwork.Elevenparttwo.java

---
+++ Seatwork.Elevenparttwo.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SEATWORKELEVENPART2");
+
+        int[][] myNumbers = { 
+            {1, 4, 2}, 
+            {3, 6, 8} 
+        };
+
+        // Print specific element
+        System.out.println(myNumbers[1][2]);
+    }
+}
