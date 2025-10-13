c66a2a18aa2ae51d4abf82150ca602b44dcf977b
sorianokiah6-beep <sorianokiah6@gmail.com>
2025-10-13 15:16:49 +0800

    Create shopping_list.java

---
+++ shopping_list.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        
+        String[] shoppingList = {"Chips", "Noodles", "Milk", "fruit", "Shampoo"};
+
+       
+        System.out.println("Your shopping list:");
+        for (int i = 0; i < shoppingList.length; i++) {
+            System.out.print(shoppingList[i]);
+            if (i < shoppingList.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println("\n\nYou entered " + shoppingList.length + " items.");
+
+        
+        System.out.print("\nSearch for an item: ");
+        String searchItem = input.nextLine();
+
+        
+        boolean found = false;
+        for (String item : shoppingList) {
+            if (item.equalsIgnoreCase(searchItem)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("✓ " + searchItem + " is in your shopping list!");
+        } else {
+            System.out.println("X " + searchItem + " is not in your shopping list.");
+        }
+
+        input.close();
+Chips uitsuitssss
