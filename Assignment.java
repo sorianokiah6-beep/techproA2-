b2e224eff5a5d92d12beec4f98b504c06ec3ccec
sorianokiah6-beep <sorianokiah6@gmail.com>
2026-02-26 20:26:37 +0800

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,18 @@
+FLOWLAYOUT
+
+import javax.swing.
+public class FlowExample
+public static void main(String[] args) {
+
+JFrame frame new JFrame('FlowLayout Example"):
+frame.setSize(300.200)
+Frame.setLayout(new java.awt.FlowLayout()).
+
+Frame.add(new JButton("Button 1")):
+frame.add(new JButton("Button 2"));
+frame.add(new JButton("Button 3"));
+
+frame.setDefaultCloseOperation(JFrame.EXIT_ON CLOSE)
+frame.setVisible(true);
+{
+{
