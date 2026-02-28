847fde2f0ddc3914fc3c30439cf470a97ed9ae4e
sorianokiah6-beep <sorianokiah6@gmail.com>
2026-02-28 20:29:20 +0800

    Create Assignmentpart2Two.java

---
+++ Assignmentpart2Two.java
@@ -0,0 +1,25 @@
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class AssignmentFour { 
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("BorderLayout Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+
+        
+        frame.setLayout(new BorderLayout());
+
+        
+        frame.add(new JButton("North"), BorderLayout.NORTH);
+        frame.add(new JButton("South"), BorderLayout.SOUTH);
+        frame.add(new JButton("East"), BorderLayout.EAST);
+        frame.add(new JButton("West"), BorderLayout.WEST);
+        frame.add(new JButton("Center"), BorderLayout.CENTER);
+
+        
+        frame.setVisible(true);
+    }
+}
